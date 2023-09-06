3e8c5a13ebddf50aba2c69a112de851451c81ce7
giirrr <ikl1121@naver.com>
2023-09-07 01:59:35 +0900

    post는 401에러-로그인 왜?

--- src/App.js
+++ src/App.js
@@ -12,14 +12,18 @@ function App() {
       </Routes>
       <button
         onClick={() => {
-          axios
-            .get("http://52.79.81.200:8080/v1/metros")
-            .then((결과) => {
-              console.log(결과.data);
-            })
-            .catch(() => {
-              console.log("실패함 ㅅㄱ");
-            });
+          // axios;
+          // .get("http://52.79.81.200:8080/v1/metros")
+          // .then((결과) => {
+          //   console.log(결과.data);
+          // })
+          // .catch(() => {
+          //   console.log("실패함 ㅅㄱ");
+          // });
+          // axios.post("http://52.79.81.200:8080/v1/login", {
+          //   email: "utest@gmail.com",
+          //   password: "1234aAcd!",
+          // }); 요거는 왜 401 에러 발생하지..
         }}
       >
         버튼
